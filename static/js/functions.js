3fdffc4737b19b0bcfce8974f65dd1dc1db8f332
Carrie Ann <carriem@thecommunityus.com>
2019-03-25 13:15:53 -0400

    updated pages
    with final content. also added favicon.

--- static/js/functions.js
+++ static/js/functions.js
@@ -10,7 +10,7 @@ $(window).ready(function() {
     gradients: true,
     autoCenter: true,
     duration: 1000,
-    pages: 14,
+    pages: 25,
     when: {
       turning: function(e, page) {
         if(page>1) {
